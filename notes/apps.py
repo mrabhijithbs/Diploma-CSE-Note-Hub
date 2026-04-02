c3a283935c0b275eea24875fd876a64a507379bd
mrabhijithbs <abibabup@gmail.com>
2026-04-02 20:20:42 +0530

    Auto-create superuser on migrate block added

--- notes/apps.py
+++ notes/apps.py
@@ -1,10 +1,16 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def create_superuser(sender, **kwargs):
+    from django.contrib.auth.models import User
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword123')
+        print("Superuser created successfully!")
 
 class NotesConfig(AppConfig):
-    name = 'notes'
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notes'
 
     def ready(self):
         import notes.signals  # This line is crucial!
+        post_migrate.connect(create_superuser, sender=self)
