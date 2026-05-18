01f1a864be073ff4e576d7c4b58972688d47fa2c
mrabhijithbs <abibabup@gmail.com>
2026-05-18 17:29:07 +0530

    fix: remove duplicate post-save signals from models.py to resolve signup IntegrityError

--- notes/models.py
+++ notes/models.py
@@ -25,18 +25,6 @@ class Profile(models.Model):
             return url.replace('raw/upload', 'image/upload')
         return url
 
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
 import re
 
 # 2. Function to determine the folder structure for uploaded PDFs
